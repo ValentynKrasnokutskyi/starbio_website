46a54a4886d7fdd9fd9a77be25baff4ca3230378
Valentyn Krasnokutskyi <kras.vvk@gmail.com>
2024-03-02 19:37:19 -0500

    Added templates and static files

--- starbio/settings.py
+++ starbio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jadlm32#q!pmg_@7xs%we0h6+d9ab+t=x1!46m(xcwsscxkqur'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'starbio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- stars/views.py
+++ stars/views.py
@@ -1,28 +1,68 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.template.loader import render_to_string
+from django.template.defaultfilters import slugify
+
+menu = [{'title': "About", 'url_name': 'about'},
+          {'title': "Add article", 'url_name': 'add_page'},
+          {'title': "Feedback", "url_name": "contact"},
+          {'title': "Login", 'url_name': 'login'}
+        ]
+
+data_db = [
+    {'id': 1, 'title': 'Angelina Jolie', 'content': '''<h1>Angelina Jolie</h1> (born Angelina Jolie[7], born Voight), formerly Jolie Pitt (born June 4, 1975, Los Angeles, California, USA) is an American film, television and voice actress, film director, screenwriter, producer, fashion model, and UN Goodwill Ambassador.
+         Winner of an Oscar, three Golden Globe Awards (the first actress in history to win the award three years in a row) and two Screen Actors Guild Awards.''',
+     'is_published': True},
+    {'id': 2, 'title': 'Margot Robbie', 'content': 'Biography of Margot Robbie', 'is_published': False},
+    {'id': 3, 'title': 'Julia Roberts', 'content': 'Biography of Julia Roberts', 'is_published': True},
+]
+
+cats_db = [
+    {'id': 1, 'name': 'Actors'},
+    {'id': 2, 'name': 'Singers'},
+    {'id': 3, 'name': 'Athletes'},
+]
 
 
 def index(request):  # HttpRequest
-    return HttpResponse("Hello, world. You're at the stars")
+    data = {
+        'title': 'Home page',
+        'menu': menu,
+        'posts': data_db,
+        'cat_selected': 0,
+    }
+    return render(request, 'stars/index.html', context=data)
 
 
-def categories(request,cat_id):  # HttpRequest
-    return HttpResponse(f"<h1> Articles by category </h1><p>id: {cat_id}</p>")
+def about(request):
+    return render(request, 'stars/about.html', {'title': 'About', 'menu': menu})
 
 
-def categories_by_slug(request, cat_slug):  # HttpRequest
-    if request.POST:
-        print(request.POST)
-    return HttpResponse(f"<h1> Articles by category </h1><p>id: {cat_slug}</p>")
+def show_post(request, post_id):
+    return HttpResponse(f"Article show with id = {post_id}")
 
 
-def archive(request, year):
-    if year > 2023:
-        url_redirect = reverse('cats', args=('music',))
-        return redirect(url_redirect)
+def addpage(request):
+    return HttpResponse("Adding an article")
 
-    return HttpResponse(f"<h1>Archive by year</h1><p >{year}</p>")
+
+def contact(request):
+    return HttpResponse("Feedback")
+
+
+def login(request):
+    return HttpResponse("Authorization")
+
+
+def show_category(request, cat_id):
+    data = {
+        'title': 'Show by category',
+        'menu': menu,
+        'posts': data_db,
+        'cat_selected': cat_id,
+    }
+    return render(request, 'stars/index.html', context=data)
 
 
 def page_not_found(request, exception):
