fdfffdcf6eeedb9d0a82bffdff64f1a78e72227f
Valentyn Krasnokutskyi <kras.vvk@gmail.com>
2024-04-22 16:46:08 -0400

    Implemented user authentication

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,14 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
 
 
-class LoginUserForm(forms.Form):
+class LoginUserForm(AuthenticationForm):
     username = forms.CharField(label='Login',
                                widget=forms.TextInput(attrs={'class': 'form-input'}))
     password = forms.CharField(label='Password',
                                widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
